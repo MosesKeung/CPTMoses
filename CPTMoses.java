2fc658eed1b5ea22a9552759568ba59b6c8db15f
MosesKeung <keungmoses4@gmail.com>
2024-12-19 09:35:24 -0500

    Create CPTMoses.java

---
+++ CPTMoses.java
@@ -0,0 +1,14 @@
+import arc.*;
+
+public class CPTMoses{
+	public static void main(String[] args){
+		Console con = new Console();
+		
+		// Variables
+		String strName;
+		
+		//Functions
+		con.println("Hello World");
+		
+	}
+}
