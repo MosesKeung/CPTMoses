fe24497d58cc354a87f7f3a52d3054632714d7d1
MosesKeung <keungmoses4@gmail.com>
2025-01-06 09:15:09 -0500

    started code

--- CPTMoses.java
+++ CPTMoses.java
@@ -5,10 +5,25 @@ public class CPTMoses{
 		Console con = new Console();
 		
 		// Variables
+		double dblChoice;
 		String strName;
 		
-		//Functions
-		con.println("Hello World");
+		//Menu Functions
+		con.println("Menu");
+		con.println("1. Play Game");
+		con.println("2. New Scores");
+		con.println("3. Quit");
+		con.println("4. Help");
+		con.println("5. Add Quiz");
+		con.println("Please Enter Option Number: ");
+		dblChoice = con.readDouble();
+		
+		
+		//Options Functions
+		if(dblChoice == 1 || dblChoice == 1.){
+			con.println("Please Enter Your Name: ");
+			strName = con.readLine();
+		}
 		
 	}
 }
