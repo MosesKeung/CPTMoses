79d66e28ec4d0433cfac5edeee4a985acafab8c1
MosesKeung <keungmoses4@gmail.com>
2025-01-06 09:40:22 -0500

    Day 1

--- CPTMoses.java
+++ CPTMoses.java
@@ -4,6 +4,12 @@ public class CPTMoses{
 	public static void main(String[] args){
 		Console con = new Console();
 		
+		//Output Files
+		
+		
+		//Input Files
+		
+		
 		// Variables
 		double dblChoice;
 		String strName;
@@ -11,7 +17,7 @@ public class CPTMoses{
 		//Menu Functions
 		con.println("Menu");
 		con.println("1. Play Game");
-		con.println("2. New Scores");
+		con.println("2. View Scores");
 		con.println("3. Quit");
 		con.println("4. Help");
 		con.println("5. Add Quiz");
@@ -23,6 +29,8 @@ public class CPTMoses{
 		if(dblChoice == 1 || dblChoice == 1.){
 			con.println("Please Enter Your Name: ");
 			strName = con.readLine();
+		}else if(dblChoice == 2 || dblChoice == 2.){
+			
 		}
 		
 	}
